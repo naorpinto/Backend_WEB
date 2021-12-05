a14d56b6b4ea17689c63a4ab1500301a825c47cb
naorpinto <naorpinto6@gmail.com>
2021-12-05 19:10:19 +0200

    ex1

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
+@app.route('/GiveByClick')
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_page_func():
+    return 'Welcome to Give By Click'
+
+
+@app.route('/About_Us')
+def about_func():
+    return 'Our Story'
+
+
+@app.route('/Our_Events')
+def our_evens_func():
+    return redirect('/About_Us')
+
+
+@app.route('/Products_page')
+def product_func():
+    return 'Our Special Products'
+
+
+@app.route('/Products_Details')
+def product_detail_func():
+    return redirect(url_for('product_func'))
+
+
+@app.route('/Donation')
+def make_donation_func():
+    return 'Donate here'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
